68379f7b9af9a331029b48348c39bdce44e8a497
Vitor Borges Jr <Vitor>
2020-12-31 18:29:39 -0300

    Create 04-Principal-Component-Analysis.R

---
+++ Linear-Algebra-for-Data-Science-in-R/04-Principal-Component-Analysis.R
@@ -0,0 +1,7 @@
+# -------------------------------------------------
+# Linear Algebra for Data Science in R - Principal Component Analysis
+# 30 dez 2020
+# VNTBJR
+# ------------------------------------------------
+# 
+# Intro to the Idea of PCA ------------------------------------------------
